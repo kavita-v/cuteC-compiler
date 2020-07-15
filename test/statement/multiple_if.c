bc699efd05f9ec773b622b1adf0e020a8c27c17d
mohitmina <34353107+mohitmina@users.noreply.github.com>
2020-07-15 18:02:00 +0530

    Update multiple_if.c

--- test/statement/multiple_if.c
+++ test/statement/multiple_if.c
@@ -2,12 +2,12 @@ int main() {
     int a = 0;
     int b = 0;
 
-    if (a)
+    if (a<2)
         a = 2;
     else
         a = 3;
 
-    if (b)
+    if (b>2)
         b = 4;
     else
         b = 5;
